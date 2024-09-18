a27107d34a501217e9115ff39d8e7d32d4af7933
Tvkharsh <89979035+Tvkharsh@users.noreply.github.com>
2024-09-19 02:50:58 +0530

    Add files via upload

---
+++ citaara/demo1.py
@@ -0,0 +1,43 @@
+print("Hello")
+
+# print 3
+
+a = 3
+print(a)
+
+# print str
+
+str = "what i need to print"
+
+print(str)
+
+b, c, d = 1, 1.2, "Great"
+e, f = 4 "good"
+print(b, c, d, e, f)
+
+# b = 1
+# print("get the value of b")
+
+print("get the value of", b)
+
+# c =1.2
+print("get the value of ", c)
+
+# concatenate
+
+# d=great
+print("{} {}".format("get the value of  " , d + " check ", b))
+
+# Checking datatype
+
+print(type(a))
+
+print(type(str))
+
+print(type(b))
+
+print(type(c))
+
+print(type(d))
+
+# print(type(f))
